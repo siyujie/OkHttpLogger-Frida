e51164c1685ab06e7f5f375fb698f3e8321762f1
siyujie <278149506@qq.com>
2020-11-26 14:23:07 +0800

    xxoo

--- okhttp_poker.js
+++ okhttp_poker.js
@@ -494,6 +494,7 @@ function hookRealCall(realCallClassName) {
             //异步
             RealCall[M_Call_enqueue].overload(Cls_CallBack).implementation = function (callback) {
                 // console.log("-------------------------------------HOOK SUCCESS 异步--------------------------------------------------")
+				var realCallBack = Java.use(callback.$className)
                 realCallBack[M_CallBack_onResponse].overload(Cls_Call,Cls_Response).implementation = function(call, response){
                     var newResponse = buildNewResponse(response)
                     this[M_CallBack_onResponse](call,newResponse)
